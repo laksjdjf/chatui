d65fdc79d8bf1361f6fe4842d0a500be07e470ab
laksjdjf <aikasama@hotmail.co.jp>
2024-07-02 20:04:25 +0900

    Update templates.py

--- tabs/templates.py
+++ tabs/templates.py
@@ -25,17 +25,23 @@ llama2 = (
 llama3 = (
     "<|start_header_id|>system<|end_header_id|>\n\n{system}<|eot_id|>",
     "<|start_header_id|>user<|end_header_id|>\n\n{user}<|eot_id|>",
-    "<|start_header_id|>chatbot<|end_header_id|>\n\n{chatbot}<|eot_id|>",
+    "<|start_header_id|>assistant<|end_header_id|>\n\n{chatbot}<|eot_id|>",
 
+)
+
+gemma = (
+    "{system}",
+    "<start_of_turn>user\n{user}<end_of_turn>\n",
+    "<start_of_turn>model\n{chatbot}<end_of_turn>\n",
 )
 
 qwen = (
-    "<|im_start|>system\n{system}<|im_end|>",
-    "<|im_start|>user\n{user}<|im_end|>",
-    "<|im_start|>assistant\n{chatbot}<|im_end|>",
+    "<|im_start|>system\n{system}<|im_end|>\n",
+    "<|im_start|>user\n{user}<|im_end|>\n",
+    "<|im_start|>assistant\n{chatbot}<|im_end|>\n",
 )
 
-template_list = ["cohere", "phi3", "swallow", "llama2", "llama3", "qwen"]
+template_list = ["cohere", "phi3", "swallow", "llama2", "llama3", "gemma", "qwen"]
 
 def get_template(template):
     if template == "cohere":
@@ -47,7 +53,9 @@ def get_template(template):
     elif template == "llama2":
         return llama2
     elif template == "llama3":
-        return llama3
+        return llama3    
+    elif template == "gemma":
+        return gemma
     elif template == "qwen":
         return qwen
     return 
