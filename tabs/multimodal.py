361536125a35a1510ffc4fbbd7cf40f041ba873b
laksjdjf <aikasama@hotmail.co.jp>
2024-07-17 19:49:13 +0900

    Update multimodal.py

--- tabs/multimodal.py
+++ tabs/multimodal.py
@@ -1,25 +1,35 @@
 import gradio as gr
 from modules.llama_process import generate_chat
 import base64
+import re
 
 def image_to_base64_data_uri(file_path):
     with open(file_path, "rb") as img_file:
         base64_data = base64.b64encode(img_file.read()).decode('utf-8')
         return f"data:image/png;base64,{base64_data}"
+
+def path2html(path):
+    return f"<img src='/file={path}' style='max-width:200px; max-height:200px'></img>"
+
+def html2path(html):
+    match = re.search(r"src='/file=([^']+)'", html)
+    if match:
+        return match.group(1)
+    return None
     
 def echo(message, history, system):
     image = None
     messages = [{"role": "system", "content": system}]
     for user, assistant in history:
-        if user is None:
-            image = image_to_base64_data_uri(assistant)
+        if assistant is None:
+            image = image_to_base64_data_uri(html2path(user))
         else:
             if image:
                 messages.append({
                     "role": "user",
                     "content": [
                         {"type" : "text", "text": user},
-                        {"type": "image_url", "image_url": {"url": image } }
+                        {"type": "image_url", "image_url": {"url": image }}
                     ]
                 })
                 image = None
@@ -41,6 +51,7 @@ def echo(message, history, system):
                 {"type": "image_url", "image_url": {"url": image } }
             ]
         })
+        history += [(path2html(message["files"][0]), None)]
     else:
         messages.append({
             "role": "user",
@@ -50,15 +61,53 @@ def echo(message, history, system):
         })
 
     content = ""
+    yield gr.update(value={"text": "", "files":[]}), history + [(message["text"], content)]
+    
     for text in generate_chat(messages):
         content += text
-        yield content
+        yield gr.update(interactive=True), history + [(message["text"], content)]
+
+def undo_history(history):
+    if len(history)>1:
+        pre_message = history[-1][0]
+        if history[-2][1] is None:
+            pre_image = [html2path(history[-2][0])]
+            pre_history = history[:-2]
+        else:
+            pre_image = []
+            pre_history = history[:-1]
+    elif len(history)==1:
+        pre_message = history[-1][0]
+        pre_image = []
+        pre_history = []
+    else:
+        pre_message = ""
+        pre_image = []
+        pre_history = []
+    return {"text": pre_message, "files": pre_image}, pre_history
+    
 
 def multimodal():
-    return gr.ChatInterface(
-        fn=echo, 
-        additional_inputs=[
-            gr.Textbox(label="system", placeholder="Type your message here...", lines=2),
-        ],
-        multimodal=True
-    )
+    with gr.Blocks() as multimodal_interface:
+        chatbot = gr.Chatbot(height=600)
+        chat_input = gr.MultimodalTextbox(interactive=True, file_types=["image"], placeholder="Enter message or upload file...", show_label=False, lines=2)
+        system = gr.Textbox(label="system", value="あなたは優秀なアシスタントです。与えられた画像について説明してください。", lines=2)
+
+        with gr.Row():
+            undo_button_chat = gr.Button("Undo")
+            clear_button = gr.ClearButton([chat_input, chatbot])
+
+        chat_input.submit(
+            echo,
+            inputs=[chat_input, chatbot, system],
+            outputs=[chat_input, chatbot]
+        )
+
+        undo_button_chat.click(
+            undo_history,
+            inputs=[chatbot],
+            outputs=[chat_input, chatbot]
+        )
+
+    return multimodal_interface
+        
